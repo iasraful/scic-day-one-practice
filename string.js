6ccc378952b66dcd665084c4545fe133e71b1aa5
Asraful Islam <iasraful321@gmail.com>
2021-05-07 01:31:53 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,16 @@
+// return 'cat'.charAt(1)
+
+// // Comparing string 
+
+let a = 'a'
+let b = 'b'
+if (a < b){
+    console.log(a +' ' + 'is less then' +  ' ' + b);
+}
+else if (a > b) {
+    console.log(a +' ' + 'is bigger than ' + ' ' + b);
+    
+}
+else{
+    console.log(a + 'and ' + b + 'are equal');
+}
