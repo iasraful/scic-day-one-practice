58cde804ae9a92a9277bc35c0d44e0c49b3e57ee
Asraful Islam <iasraful321@gmail.com>
2021-05-07 12:51:24 +0600

    day one

--- string.js
+++ string.js
@@ -2,15 +2,38 @@
 
 // // Comparing string 
 
-let a = 'a'
-let b = 'b'
-if (a < b){
-    console.log(a +' ' + 'is less then' +  ' ' + b);
-}
-else if (a > b) {
-    console.log(a +' ' + 'is bigger than ' + ' ' + b);
+// let a = 'a'
+// let b = 'b'
+// if (a < b){
+//     console.log(a +' ' + 'is less then' +  ' ' + b);
+// }
+// else if (a > b) {
+//     console.log(a +' ' + 'is bigger than ' + ' ' + b);
     
-}
-else{
-    console.log(a + 'and ' + b + 'are equal');
-}
+// }
+// else{
+//     console.log(a + 'and ' + b + 'are equal');
+// }
+
+//     Use of String .chartAt
+
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+// const index = 4;
+
+// console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+
+// Use of concat();
+
+// const firstName = 'Asraful';
+// const lastName = 'Islam';
+
+// console.log(firstName.concat(',' , lastName));
+
+
+// USE of String.prototype.includes()
+
+const firstSentence = 'Amar Naam Ashraf'
+const searchingWord = 'naam'
+console.log(`The word "${searchingWord}" ${firstSentence.includes(searchingWord) ? 'is ' : 'is not'} in the line `);
+
